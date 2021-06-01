0c13dac4a7b65e6cccf10a991d85f0b67df07abe
PetrovSergeyAleksandrovich <info@poizone.ru>
2021-06-01 11:02:24 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Computer_Simulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Computer_Simulator.dir/main.cpp.obj"
+  "Computer_Simulator.exe"
+  "Computer_Simulator.exe.manifest"
+  "Computer_Simulator.lib"
+  "Computer_Simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Computer_Simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
